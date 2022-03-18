b736c7eadf43aba51df580af5950c52ec5b9f664
True-Slimen <58563165+True-Slimen@users.noreply.github.com>
2022-03-18 10:07:37 +0100

    page face

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
+import Face from '../views/Face.vue'
 
 Vue.use(VueRouter)
 
@@ -27,6 +28,11 @@ const routes = [
         path: '/dashboard',
         name: 'Dashboard',
         component: Dashboard
+    },
+    {
+        path: '/face',
+        name: 'Face',
+        component: Face
     }
 ]
 
---
+++ src/views/Face.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="face">
+    face
+    <button @click="postPicture()">lol</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Face",
+  data() {
+    return {
+      user: {},
+    }
+  },
+  created(){
+    console.log('toto');
+    // Promise.all([
+    //   faceapi.nets.faceRecognitionNet.loadFromUri('../../public/face/models'),
+    //   faceapi.nets.faceLandmark68Net.loadFromUri('../../public/face/models'),
+    //   faceapi.nets.ssdMobilenetv1.loadFromUri('../../public/face/models')    
+    // ]).then(this.start);
+  },
+  mounted() {
+    if (localStorage) {
+      this.user = JSON.parse(localStorage.getItem("user"));
+      
+      if(!this.user.token){
+        this.$router.push({ name: 'Login' })
+      }
+    }else{
+        this.$router.push({ name: 'Login' })
+    }
+  },
+  methods: {
+    postPicture() {
+        console.log('post image');
+        this.axios
+        .post("http://localhost:3000/api/face/post", {
+
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+  }
+};
+</script>
+
+<style>
+#faceWarp{
+  margin: 0 auto;
+  padding: 0;
+  width: 500px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+canvas{
+  position: absolute;
+
+}
+</style>
